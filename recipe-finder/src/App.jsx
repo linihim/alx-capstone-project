b5d5ed59e9346d9f1a696d5b5f4c8fcf9f7c592f
Lindah <linihim03@gmail.com>
2024-10-13 01:08:49 +0300

    Setup routing.

--- recipe-finder/src/App.jsx
+++ recipe-finder/src/App.jsx
@@ -1,31 +1,17 @@
-import React, { useState }  from 'react'
-import SearchBar from './components/SearchBar'
-import RecipeCard from './components/Recipecard'
-import { searchRecipes } from './services/api'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
-    const [recipes, setRecipes] = useState([]);
-
-    const handleSearch = async (query) => {
-      try {
-        const results = await searchRecipes(query);
-        setRecipes(results || []);
-      } catch (error) {
-        console.error('Error searching recipes:', error);
-      }
-    };
-
   return (
-    <div className="min-h-screen bg-gray-100 p-8">
-      <h1 className="text-3xl font-bold mb-4 text-center"> Yum 
-        Recipe Finder</h1>
-      <SearchBar onSearch={handleSearch} />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.idMeal} recipe={recipe} />
-        ))}
+    <Router>
+       <div className="min-h-screen bg-gray-100">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-    </div>
+
+    </Router>
   );
 }
 
--- recipe-finder/src/pages/Home.jsx
+++ recipe-finder/src/pages/Home.jsx
@@ -1,3 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import SearchBar from '../components/SearchBar';
-import { searchRecipes } from '../services/api';
+import React, { useState } from 'react'
+import SearchBar from '../components/SearchBar'
+import RecipeCard from '../components/Recipecard'
+import { searchRecipes } from '../services/api'
+
+const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+
+  const handleSearch = async (query) => {
+    try {
+      const results = await searchRecipes(query);
+      setRecipes(results || []);
+    } catch (error) {
+      console.error('Error searching recipes:', error);
+    }
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-8 text-center">Recipe Finder</h1>
+      <SearchBar onSearch={handleSearch} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.idMeal} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
