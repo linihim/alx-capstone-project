2d8d11cdf5c080c4868e0a5090871c66686c4afe
Lindah <linihim03@gmail.com>
2024-10-13 12:16:36 +0300

    Include a link to the recipe details in the RecipeCard component.

--- recipe-finder/src/components/Recipecard.jsx
+++ recipe-finder/src/components/Recipecard.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React from 'react'
+import {Link} from 'react-router-dom'
 
 const RecipeCard = ({ recipe }) => {
   return (
+    <Link to={`/recipe/${recipe.idMeal}`} className="block">
     <div className="border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
       <img src={recipe.strMealThumb} alt={recipe.strMeal} className="w-full h-48 object-cover" />
       <div className="p-4">
@@ -10,6 +12,7 @@ const RecipeCard = ({ recipe }) => {
         <p className="text-gray-600">Cuisine: {recipe.strArea}</p>
       </div>
     </div>
+    </Link>
   );
 };
 
