d4ea56574c56c12eaa25ce449e2672adaad2ca02
Lindah <linihim03@gmail.com>
2024-10-13 12:24:08 +0300

    Include the recipe route in App.jsx.

--- recipe-finder/src/App.jsx
+++ recipe-finder/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import recipe from './pages/Recipe'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
        <div className="min-h-screen bg-gray-100">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/recipe/:id" element={<recipe />} />
         </Routes>
       </div>
 
