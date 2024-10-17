6cf00c8a0e9247dfd1c48fef8632a836e30a848a
Lindah <linihim03@gmail.com>
2024-10-17 10:22:02 +0300

    update background image path.

--- recipe-finder/src/App.jsx
+++ recipe-finder/src/App.jsx
@@ -6,7 +6,7 @@ import Recipe from './pages/Recipe'
 function App() {
   return (
     <Router>
-       <div className="min-h-screen bg-gray-100 bg-cover bg-center bg-fixed" style={{backgroundImage: "url('https://assets.epicurious.com/photos/62d6c513077a952f4a8c338c/4:3/w_3668,h_2751,c_limit/PannaCotta_RECIPE_04142022_9822_final.jpg')"}}>
+       <div className="min-h-screen bg-gray-100 bg-cover bg-center bg-fixed" style={{backgroundImage:"url('/capstone-bg.jpg')"}}>
         <div className="min-h-screen bg-white bg-opacity-90">
         <Routes>
             <Route path="/" element={<Home />} />
