572e34f4e56812ac8e25f8df154eddd316243516
Lindah <linihim03@gmail.com>
2024-10-17 11:08:59 +0300

    Import Navigate component from react-router-dom.

--- recipe-finder/src/App.jsx
+++ recipe-finder/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Recipe from './pages/Recipe'
+import { Navigate } from 'react-router-dom'
 
 function App() {
   return (
